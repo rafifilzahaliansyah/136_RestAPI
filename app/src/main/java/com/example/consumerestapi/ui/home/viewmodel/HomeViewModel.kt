cd09d4d39bd5b7867cff55c6aecfc43640706c21
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2023-12-28 18:23:36 +0700

    membuat class HomeViewModel

--- app/src/main/java/com/example/consumerestapi/ui/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/viewmodel/HomeViewModel.kt
@@ -1,9 +1,19 @@
 package com.example.consumerestapi.ui.home.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.consumerestapi.model.Kontak
+import com.example.consumerestapi.repository.KontakRepository
 
 sealed class KontakUIState {
     data class Success(val kontak: List<Kontak>) : KontakUIState()
     object Error : KontakUIState()
     object Loading : KontakUIState()
+}
+
+class HomeViewModel(private val kontakRepository: KontakRepository) : ViewModel() {
+    var kontakUIState: KontakUIState by mutableStateOf(KontakUIState.Loading)
+        private set
 }
