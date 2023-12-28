b0170822498054c0d225634e9d16f21dea11c72b
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2023-12-28 18:20:50 +0700

    membuat class KontakUIState

--- app/src/main/java/com/example/consumerestapi/ui/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/viewmodel/HomeViewModel.kt
@@ -1,2 +1,9 @@
 package com.example.consumerestapi.ui.home.viewmodel
 
+import com.example.consumerestapi.model.Kontak
+
+sealed class KontakUIState {
+    data class Success(val kontak: List<Kontak>) : KontakUIState()
+    object Error : KontakUIState()
+    object Loading : KontakUIState()
+}
